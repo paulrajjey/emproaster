2554760fee9a169cce706daba2afc6705ed6b8f2
rhdmAdmin <>
2019-08-01 16:29:05 -0500

    {/src/main/java/com/saq/emproaster/Employee.java}

--- src/main/java/com/saq/emproaster/Employee.java
+++ src/main/java/com/saq/emproaster/Employee.java
@@ -6,12 +6,33 @@ package com.saq.emproaster;
 
 public class Employee implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Employee() {
-    }
+	private java.lang.String code;
+	private java.lang.String name;
 
+	public Employee() {
+	}
 
+	public java.lang.String getCode() {
+		return this.code;
+	}
 
+	public void setCode(java.lang.String code) {
+		this.code = code;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Employee(java.lang.String code, java.lang.String name) {
+		this.code = code;
+		this.name = name;
+	}
 
 }
