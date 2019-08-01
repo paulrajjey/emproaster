cf6de65791dc22ec71816ab8deafe2f8159b9612
rhdmAdmin <>
2019-08-01 16:58:53 -0500

    {/src/main/java/com/saq/emproaster/ShiftOnRequest.java}

--- src/main/java/com/saq/emproaster/ShiftOnRequest.java
+++ src/main/java/com/saq/emproaster/ShiftOnRequest.java
@@ -6,12 +6,44 @@ package com.saq.emproaster;
 
 public class ShiftOnRequest implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ShiftOnRequest() {
-    }
+	private com.saq.emproaster.Employee employee;
+	private com.saq.emproaster.Shift shift;
+	private int weight;
 
+	public ShiftOnRequest() {
+	}
 
+	public com.saq.emproaster.Employee getEmployee() {
+		return this.employee;
+	}
 
+	public void setEmployee(com.saq.emproaster.Employee employee) {
+		this.employee = employee;
+	}
+
+	public com.saq.emproaster.Shift getShift() {
+		return this.shift;
+	}
+
+	public void setShift(com.saq.emproaster.Shift shift) {
+		this.shift = shift;
+	}
+
+	public int getWeight() {
+		return this.weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public ShiftOnRequest(com.saq.emproaster.Employee employee,
+			com.saq.emproaster.Shift shift, int weight) {
+		this.employee = employee;
+		this.shift = shift;
+		this.weight = weight;
+	}
 
 }
