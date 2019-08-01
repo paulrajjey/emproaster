b9a34284bd34090bc43b6583dda25244effb9dda
rhdmAdmin <>
2019-08-01 16:47:10 -0500

    {/src/main/java/com/saq/emproaster/Shift.java}

--- src/main/java/com/saq/emproaster/Shift.java
+++ src/main/java/com/saq/emproaster/Shift.java
@@ -6,12 +6,34 @@ package com.saq.emproaster;
 
 public class Shift implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Shift() {
-    }
+	private com.saq.emproaster.ShiftDate shiftDate;
+	private int requiredEmployeeSize;
 
+	public Shift() {
+	}
 
+	public com.saq.emproaster.ShiftDate getShiftDate() {
+		return this.shiftDate;
+	}
 
+	public void setShiftDate(com.saq.emproaster.ShiftDate shiftDate) {
+		this.shiftDate = shiftDate;
+	}
+
+	public int getRequiredEmployeeSize() {
+		return this.requiredEmployeeSize;
+	}
+
+	public void setRequiredEmployeeSize(int requiredEmployeeSize) {
+		this.requiredEmployeeSize = requiredEmployeeSize;
+	}
+
+	public Shift(com.saq.emproaster.ShiftDate shiftDate,
+			int requiredEmployeeSize) {
+		this.shiftDate = shiftDate;
+		this.requiredEmployeeSize = requiredEmployeeSize;
+	}
 
 }
