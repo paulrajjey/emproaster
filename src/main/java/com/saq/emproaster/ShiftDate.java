775c35b7d3a85e59c479c1bd5c74a181920049c6
rhdmAdmin <>
2019-08-01 16:37:31 -0500

    {/src/main/java/com/saq/emproaster/ShiftDate.java}

--- src/main/java/com/saq/emproaster/ShiftDate.java
+++ src/main/java/com/saq/emproaster/ShiftDate.java
@@ -6,12 +6,33 @@ package com.saq.emproaster;
 
 public class ShiftDate implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ShiftDate() {
-    }
+	private int dayIndex;
+	private java.lang.String dateString;
 
+	public ShiftDate() {
+	}
 
+	public int getDayIndex() {
+		return this.dayIndex;
+	}
 
+	public void setDayIndex(int dayIndex) {
+		this.dayIndex = dayIndex;
+	}
+
+	public java.lang.String getDateString() {
+		return this.dateString;
+	}
+
+	public void setDateString(java.lang.String dateString) {
+		this.dateString = dateString;
+	}
+
+	public ShiftDate(int dayIndex, java.lang.String dateString) {
+		this.dayIndex = dayIndex;
+		this.dateString = dateString;
+	}
 
 }
